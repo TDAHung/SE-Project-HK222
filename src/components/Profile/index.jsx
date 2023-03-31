ac6d10a8659ae6d14dc296cd4f84abfb56f518ec
= <=>
2023-03-31 13:10:27 +0700

    fix conflict

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -1,14 +1,9 @@
 const Profile = () =>{
-<<<<<<< HEAD
     return (
         <div>
             Profile
         </div>
     );
 };
-=======
-    return <div>Profile</div>
-}
->>>>>>> 1d91fce3ac604846fb6d2c9c05655d9a94e5e19b
 
 export default Profile;
