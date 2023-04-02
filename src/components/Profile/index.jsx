b832ca8c559e623680bbf76e41f37a433bc8f899
ZaPhat <93189118+Zaphat@users.noreply.github.com>
2023-04-02 15:48:37 +0700

    add-profile-placeholder
    require API call to get user info later.

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -1,7 +1,16 @@
-const Profile = () =>{
+import React from 'react';
+import './Profile.css';
+const Profile = () => {
     return (
-        <div>
-            Profile
+        <div className="popup">
+            <div className="profile-body">
+              <div> <img className="profile-img" src="https://helios-i.mashable.com/imagery/articles/03BFwHOQ1dzjtArO1cmYU50/hero-image.fill.size_1248x702.v1628521216.jpg" alt="Profile"/></div> 
+                <div className="profile-info">
+                    <p className="profile-name">John Doe</p>
+                    <p className="profile-email">johndoe@example.com</p>
+                    <p className="profile-bio">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ac enim quis risus fringilla congue ac id metus.</p>
+                </div>
+            </div>
         </div>
     );
 };
