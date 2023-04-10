5083ba5cd66775607497d0f4a10b1afc1193cb70
= <=>
2023-04-10 20:37:58 +0700

    set Modal

--- src/components/Schedule/index.jsx
+++ src/components/Schedule/index.jsx
@@ -1,16 +1,24 @@
 import React, { useEffect, useState } from "react";
-import { Badge, Calendar } from 'antd';
+import { Badge, Calendar, Modal, Button } from 'antd';
 
 //import style
 import "./Schedule.css";
+import gas from "../../assets/images/gas-pump-solid.png";
+import weight from "../../assets/images/weight-scale-solid.png";
+import strength from "../../assets/images/hand-fist-solid.png"
+import location from "../../assets/images/location-crosshairs-solid.png";
 
 //import controller
 import User from "../../controller/user/userController";
 import Task from "../../controller/task/taskController";
 
+
+
 const Schedule = () =>{
     const [userData,setUserData] = useState([]);
     const [taskData,setTaskData] = useState([]);
+    const [modal,setModal] = useState(false);
+    const [idTask, setIDTask] = useState("");
 
     useEffect(()=>{
         const fetch = async () =>{
@@ -42,7 +50,6 @@ const Schedule = () =>{
             const year = String(element.date).substring(String(element.date).lastIndexOf('-')+1,String(element.date).length);
             return value.month() + 1 === Number(month) && value.date() === Number(day) && value.year() === Number(year);
         });
-        console.log(deadline);
         return deadline;
     };
 
@@ -52,7 +59,7 @@ const Schedule = () =>{
         <ul className="events">
             {listData.map((item) => (
             <li key={item.id}>
-                <Badge>
+                <Badge onClick={()=>{setModal(true); setIDTask(item.id)}}>
                     <div className="calendar__user">
                         <div className="calendar__img"><img src={item.user.imgUrl} alt="" /></div>
                         <div className="calendar__name">{item.user.name}</div>
@@ -69,6 +76,38 @@ const Schedule = () =>{
         <Calendar 
             dateCellRender={dateCellRender}
         />;
+        <Modal
+            open={modal}
+            cancelText={null}
+            closable={false}
+            confirmLoading={true}
+            footer={<div className='modal__footer'>
+                <Button className="modal__cancel" onClick={()=>{setModal(false)}}>Cancel</Button>
+            </div>}
+            className="modal"
+        >
+            <div className="modal__task__description">
+                <div className="modal__task">
+                        <img src={location} alt="" />
+                        Address: TPHCM
+                </div>
+                <div className="line"></div>
+                <div>
+                    <div className="modal__task">
+                        <img src={weight} alt="" />
+                        Weight(Tons): 140
+                    </div>
+                    <div className="modal__task">
+                        <img src={strength} alt="" />
+                        Capacity(Tons): 5
+                    </div>
+                    <div className="modal__task">
+                        <img src={gas} alt="" />
+                        Fuel Consumption(Litre): 10
+                    </div>
+                </div>
+            </div>
+        </Modal>
     </div>
 }
 
